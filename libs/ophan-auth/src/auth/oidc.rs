fa3658d14f4d64da1521a539985fb40a1c584fb1
Alex <zsweiter@gmail.com>
2026-08-14 04:16:26 -0500

    update oidc configuration model

--- libs/ophan-auth/src/auth/oidc.rs
+++ libs/ophan-auth/src/auth/oidc.rs
@@ -2,14 +2,30 @@ use serde::Deserialize;
 
 use crate::crypto::SignatureAlg;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct OidcConfiguration {
     pub issuer: String,
-    pub authorization_endpoint: String,
     pub token_endpoint: String,
     pub jwks_uri: String,
     pub response_types_supported: Vec<String>,
     pub subject_types_supported: Vec<String>,
+
+    #[serde(default)]
     pub id_token_signing_alg_values_supported: Vec<SignatureAlg>,
-    pub dpop_signing_alg_values_supported: Vec<SignatureAlg>,
+
+    #[serde(default)]
+    pub authorization_endpoint: Option<String>,
+
+    /// RFC 9449: Demonstration of Proof-of-Possession (DPoP)
+    #[serde(default)]
+    pub dpop_signing_alg_values_supported: Option<Vec<SignatureAlg>>,
+
+    #[serde(default)]
+    pub scopes_supported: Option<Vec<String>>,
+
+    #[serde(default)]
+    pub grant_types_supported: Option<Vec<String>>,
+
+    #[serde(default)]
+    pub token_endpoint_auth_methods_supported: Option<Vec<String>>,
 }
